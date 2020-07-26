507eb8cc6690c8fd2445ac79412a7631e0a1434b
Dionathan Cordova <dionathan.cordova@vivitech.com.br>
2020-07-26 18:17:12 -0300

    fim do desafio

--- desafio02-conceitos-nodejs/src/app.js
+++ desafio02-conceitos-nodejs/src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,67 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = { id: uuid(), title, url, techs, likes: 0};
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repoIndex = repositories.findIndex(repository => repository.id === id);
+
+  if( repoIndex < 0 ) {
+    return response.status(400).json({'error':'Error on catch the repository'});
+  }
+
+  const updateRepository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repoIndex].likes
+  }
+
+  repositories[repoIndex] = updateRepository;
+
+  return response.json(updateRepository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const findIndex = repositories.findIndex(repo => repo.id === id);
+
+  if( findIndex < 0 ) {
+    return response.status(400).json({'error':'Error on catch the repository'});
+  }
+
+  repositories.splice(findIndex, 1);
+
+  return response.status(204).json({'OK': 'Repository removed'})
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const findIndex = repositories.findIndex(repo => repo.id === id);
+
+  if( findIndex < 0 ) {
+    return response.status(400).json({'error':'Error on catch the repository'});
+  }
+
+  repositories[findIndex].likes++;
+
+  return response.status(200).json(repositories[findIndex]);
 });
 
 module.exports = app;
